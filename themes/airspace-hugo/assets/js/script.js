3b5dca94fb3ac2ceb788f7426d309aa86446431d
Alex Bobkov <lilalex85@gmail.com>
2021-05-28 22:07:35 +0300

    optimization part 1

--- themes/airspace-hugo/assets/js/script.js
+++ themes/airspace-hugo/assets/js/script.js
@@ -1,27 +1,5 @@
-// Preloader js
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
-});
-
 $(document).ready(function () {
 	'use strict';
-
-	// Shuffle js filter and masonry
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
-
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
 
 	$('.testimonial-slider').slick({
 		slidesToShow: 1,
@@ -31,15 +9,15 @@ $(document).ready(function () {
 		autoplaySpeed: 4000
 	});
 
-  // Turn cloaked e-mail addresses into clickable mailto links
-  let emailSpans = document.getElementsByClassName("cloaked-e-mail");
+	// Turn cloaked e-mail addresses into clickable mailto links
+    let emailSpans = document.getElementsByClassName("cloaked-e-mail");
 
-  for (let emailSpan of emailSpans) {
-    let emailLink = document.createElement("a");
-    let emailAddress = emailSpan.attributes.getNamedItem("data-user").value.split('').reverse().join('') + "@" + emailSpan.attributes.getNamedItem("data-domain").value.split('').reverse().join('');
-    emailLink.href = "mailto:" + emailAddress;
-    emailLink.innerText = emailAddress;
-    emailSpan.parentElement.insertBefore(emailLink, emailSpan);
-    emailSpan.parentElement.removeChild(emailSpan)
-  }
+    for (let emailSpan of emailSpans) {
+        let emailLink = document.createElement("a");
+        let emailAddress = emailSpan.attributes.getNamedItem("data-user").value.split('').reverse().join('') + "@" + emailSpan.attributes.getNamedItem("data-domain").value.split('').reverse().join('');
+        emailLink.href = "mailto:" + emailAddress;
+        emailLink.innerText = emailAddress;
+        emailSpan.parentElement.insertBefore(emailLink, emailSpan);
+        emailSpan.parentElement.removeChild(emailSpan)
+    }
 });
